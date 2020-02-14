deb7c18c3519b8e4edfe9f354eaa70a1a1445172
ElkanRoelen <roelen@elkan.nl>
2020-02-14 16:58:50 +0100

    Test with cascades

--- cameraTest.py
+++ cameraTest.py
@@ -9,7 +9,6 @@ import argparse
 import time
 import cv2
 
-
 # construct the argument parse and parse the arguments
 from utils.fps import FPS
 from utils.stream import WebcamVideoStream
@@ -24,7 +23,9 @@ args = vars(ap.parse_args())
 
 # load our serialized model from disk
 print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+face_cascade = cv2.CascadeClassifier('models/lbpcascade_frontalface_improved.xml')
+# net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 vs = WebcamVideoStream(src=0).start()
@@ -41,36 +42,46 @@ while True:
 
     # grab the frame dimensions and convert it to a blob
     (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-                                 (300, 300), (104.0, 177.0, 123.0))
 
-    # pass the blob through the network and obtain the detections and
-    # predictions
-    net.setInput(blob)
-    detections = net.forward()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # loop over the detections
-    for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with the
-        # prediction
-        confidence = detections[0, 0, i, 2]
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
-        if confidence < args["confidence"]:
-            continue
-        # compute the (x, y)-coordinates of the bounding box for the
-        # object
-        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        (startX, startY, endX, endY) = box.astype("int")
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
 
-        # draw the bounding box of the face along with the associated
-        # probability
-        text = "{:.2f}%".format(confidence * 100)
-        y = startY - 10 if startY - 10 > 10 else startY + 10
-        cv2.rectangle(frame, (startX, startY), (endX, endY),
-                      (0, 0, 255), 2)
-        cv2.putText(frame, text, (startX, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    #
+    # blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+    #                              (300, 300), (104.0, 177.0, 123.0))
+    #
+    # # pass the blob through the network and obtain the detections and
+    # # predictions
+    # net.setInput(blob)
+    # detections = net.forward()
+
+    # # loop over the detections
+    # for i in range(0, detections.shape[2]):
+    #     # extract the confidence (i.e., probability) associated with the
+    #     # prediction
+    #     confidence = detections[0, 0, i, 2]
+    #     # filter out weak detections by ensuring the `confidence` is
+    #     # greater than the minimum confidence
+    #     if confidence < args["confidence"]:
+    #         continue
+    #     # compute the (x, y)-coordinates of the bounding box for the
+    #     # object
+    #     box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+    #     (startX, startY, endX, endY) = box.astype("int")
+    #
+    #     # draw the bounding box of the face along with the associated
+    #     # probability
+    #     text = "{:.2f}%".format(confidence * 100)
+    #     y = startY - 10 if startY - 10 > 10 else startY + 10
+    #     cv2.rectangle(frame, (startX, startY), (endX, endY),
+    #                   (0, 0, 255), 2)
+    #     cv2.putText(frame, text, (startX, y),
+    #                 cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
     # show the output frame
     cv2.imshow("Didux.io", frame)
